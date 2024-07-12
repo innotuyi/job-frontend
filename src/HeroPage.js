8b3695a26958513768796b5e9ef561d51b5c5ff5
TUYISHIMIRE Innocent <innocent.tuyishimire@parsenate.local>
2024-07-12 11:22:12 +0200

    updaitng homepage

--- src/HeroPage.js
+++ src/HeroPage.js
@@ -21,6 +21,42 @@ const HeroPage = () => {
       <NavLink to="/PostAdvert">
         <a className="btn btn-primary">Start Advertising</a>
       </NavLink>
+
+     <div class="container-fluid bg-primary mb-5 mt-5 wow fadeIn" data-wow-delay="0.1s" style={{
+
+        "padding": "35px"
+      }}>
+        <div class="container">
+          <div class="row g-2">
+            <div class="col-md-10">
+              <div class="row g-2">
+                <div class="col-md-4">
+                  <input type="text" class="form-control border-0" placeholder="Keyword" />
+                </div>
+                <div class="col-md-4">
+                  <select class="form-select border-0">
+                    <option selected>Category</option>
+                    <option value="1">Marketing</option>
+                    <option value="2">Project Management</option>
+                    <option value="3">Business</option>
+                  </select>
+                </div>
+                <div class="col-md-4">
+                  <select class="form-select border-0">
+                    <option selected>Location</option>
+                    <option value="1">Location 1</option>
+                    <option value="2">Location 2</option>
+                    <option value="3">Location 3</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-2">
+              <button class="btn btn-dark border-0 w-100">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,14 @@ import Job from './Job'
 import Hero from './HeroPage'
 import Footer from './Footer'
 import SearchComponent from './SearchComponent'
+import Category from './Categories'
 
 function Home() {
   return (
     <>
     <Nav/>
     <Hero/>
+    <Category/>
     <Job/>
     <Footer/>
     <SearchComponent/>
